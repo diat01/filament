26faf7cb10cf30d81551ed3c68817ffa3a83f513
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 18:27:03 +0500

    create turkmen layout.php

---
+++ packages/panels/resources/lang/tk/layout.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+
+    'direction' => 'ltr',
+
+    'actions' => [
+
+        'billing' => [
+            'label' => 'Abunalygy dolandyrmak',
+        ],
+
+        'logout' => [
+            'label' => 'Hasapdan çykmak',
+        ],
+
+        'open_database_notifications' => [
+            'label' => 'Bildirişleri aç',
+        ],
+
+        'open_user_menu' => [
+            'label' => 'Ulanyjy menýusy',
+        ],
+
+        'sidebar' => [
+
+            'collapse' => [
+                'label' => 'Çep panelini gysgalt',
+            ],
+
+            'expand' => [
+                'label' => 'Çep panelini giňelt',
+            ],
+
+        ],
+
+        'theme_switcher' => [
+
+            'dark' => [
+                'label' => 'Garaňky temany işlet',
+            ],
+
+            'light' => [
+                'label' => 'Ýagty temany işlet',
+            ],
+
+            'system' => [
+                'label' => 'Ulgam temany işlet',
+            ],
+
+        ],
+
+    ],
+
+    'avatar' => [
+        'alt' => ':name avatary',
+    ],
+
+    'logo' => [
+        'alt' => ':name logotipi',
+    ],
+
+];
