335d28f7c227cdde0b58c295447d079c4196c9d6
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 18:20:55 +0500

    create turkmen global-search.php

---
+++ packages/panels/resources/lang/tk/global-search.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'field' => [
+        'label' => 'Global gözleg',
+        'placeholder' => 'Gözleg',
+    ],
+
+    'no_results_message' => 'Hiç bir gözleg netije tapylmady.',
+
+];
