7cefac0395e161c0e76094f4a97cb20ba5571c98
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:05:18 +0500

    create turkmen edit-record.php

---
+++ packages/panels/resources/lang/tk/resources/pages/edit-record.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'title' => ':label üýtget',
+
+    'breadcrumb' => 'Üýtget',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'Ýatyr',
+            ],
+
+            'save' => [
+                'label' => 'Üýtgeşmeleri ýatda sakla',
+            ],
+
+        ],
+
+    ],
+
+    'content' => [
+
+        'tab' => [
+            'label' => 'Üýtget',
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => 'Üýtgedildi',
+        ],
+
+    ],
+
+];
