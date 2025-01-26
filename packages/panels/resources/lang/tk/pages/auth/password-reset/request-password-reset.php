543cf9b5bddebce0574516b025d837884931167d
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:29:48 +0500

    create turkmen request-password-reset.php

---
+++ packages/panels/resources/lang/tk/pages/auth/password-reset/request-password-reset.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'title' => 'Açar sözüňizi täzeden düzüň',
+
+    'heading' => 'Açar sözüňizi ýatdan çykardyňyzmy?',
+
+    'actions' => [
+
+        'login' => [
+            'label' => 'girişe dolan',
+        ],
+
+    ],
+
+    'form' => [
+
+        'email' => [
+            'label' => 'E-poçta adresi',
+        ],
+
+        'actions' => [
+
+            'request' => [
+                'label' => 'Poçta ugrat',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'throttled' => [
+            'title' => 'Gaty köp synanyşyk',
+            'body' => ':seconds sekuntdan soňra täzeden synanyşyň.',
+        ],
+
+    ],
+
+];
