14ae957377d37dee0c01ba0661d88ef537a9390a
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:43:02 +0500

    create turkmen email-verification-prompt.php

---
+++ packages/panels/resources/lang/tk/pages/auth/email-verification/email-verification-prompt.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    'title' => 'E-poçta salgyňyzy dogrulaň',
+
+    'heading' => 'E-poçta salgyňyzy dogrulaň',
+
+    'actions' => [
+
+        'resend_notification' => [
+            'label' => 'Täzeden ugrat',
+        ],
+
+    ],
+
+    'messages' => [
+        'notification_not_received' => 'Biziň ugradan e-poçtamyzy almadyňyzmy?',
+        'notification_sent'         => 'E-poçta salgyňyzy nädip dogrulamalydygy barada görkezmeleri :email e-poçta salgysyna ugratdyk.',
+    ],
+
+    'notifications' => [
+
+        'notification_resent' => [
+            'title' => 'E-poçtany täzeden ugratdyk.',
+        ],
+
+        'notification_resend_throttled' => [
+            'title' => 'Gaty köp synanyşyk',
+            'body'  => ':seconds sekuntdan soňra täzeden synanyşyň.',
+        ],
+
+    ],
+
+];
