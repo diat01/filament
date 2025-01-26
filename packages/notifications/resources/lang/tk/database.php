7dd550de9b87e8ccedf0692e37282d259658ed36
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:45:29 +0500

    create turkmen database.php

---
+++ packages/notifications/resources/lang/tk/database.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'modal' => [
+
+        'heading' => 'Bildirişler',
+
+        'actions' => [
+
+            'clear' => [
+                'label' => 'Arassala',
+            ],
+
+            'mark_all_as_read' => [
+                'label' => 'Hemmesini okaldy diýip belle',
+            ],
+
+        ],
+
+        'empty' => [
+            'heading' => 'Bildiriş ýok',
+            'description' => 'Soňrak barlamagyňyzy haýyş edýäris.',
+        ],
+
+    ],
+
+];
