e7c4eaa27edd85ad64bf273b2c3ba175882d71de
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:20:42 +0500

    create turkmen edit-profile.php

---
+++ packages/panels/resources/lang/tk/pages/auth/edit-profile.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    'label' => 'Profil',
+
+    'form' => [
+
+        'email' => [
+            'label' => 'E-poçta adresi',
+        ],
+
+        'name' => [
+            'label' => 'At',
+        ],
+
+        'password' => [
+            'label' => 'Täze açar söz',
+        ],
+
+        'password_confirmation' => [
+            'label' => 'Täze açar sözi tassykla',
+        ],
+
+        'actions' => [
+
+            'save' => [
+                'label' => 'Üýtgeşmeleri ýatda sakla',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => 'Üýtgedildi',
+        ],
+
+    ],
+
+    'actions' => [
+
+        'cancel' => [
+            'label' => 'Ýatyr',
+        ],
+
+    ],
+
+];
