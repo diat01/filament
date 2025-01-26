50c73b41758499999cf21abb17c38ca7173b9b01
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:12:34 +0500

    create turkmen dashboard.php

---
+++ packages/panels/resources/lang/tk/pages/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'title' => 'Maglumat paneli',
+
+    'actions' => [
+
+        'filter' => [
+
+            'label' => 'Süzgüç',
+
+            'modal' => [
+
+                'heading' => 'Süzgüç',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => 'Apply',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
+
+];
