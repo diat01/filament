3d01a442e89249015af24aed8a38d0be9af05dd5
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-01-26 20:05:09 +0500

    create turkmen create-record.php

---
+++ packages/panels/resources/lang/tk/resources/pages/create-record.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'title' => ':label döret',
+
+    'breadcrumb' => 'Döret',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'Ýatyr',
+            ],
+
+            'create' => [
+                'label' => 'Döret',
+            ],
+
+            'create_another' => [
+                'label' => 'Döret & täze döret',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'created' => [
+            'title' => 'Döredildi',
+        ],
+
+    ],
+
+];
